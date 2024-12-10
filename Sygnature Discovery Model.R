eadbe6960a534ce8a69b18f9b2e05c2876df16ac
Ruth399 <66483864+Ruth399@users.noreply.github.com>
2024-12-10 14:26:45 +0000

    Update Sygnature Discovery Model.R

--- Sygnature Discovery Model.R
+++ Sygnature Discovery Model.R
@@ -21,12 +21,11 @@ data <- data.frame(time = time,
 
 ggplot(data , aes(x = time)) +
   geom_line(aes(y = glucocorticoid_levels, color = "Glucocorticoid Receptor Expression"), size = 1.2) +
-  geom_line(aes(y = metabolism_levels, color = "Metabolism-Related Gene Expression"), linetype = "dashed", size = 1.2) +
   labs(title = "Simulated Glucocorticoid Receptor and Metabolism Gene Expression Over 24 Hours",
        x = "Time of Day (hours)", 
        y = "Expression Level (Arbitrary Units)") +
-  scale_color_manual(values = c("Glucocorticoid Receptor Expression" = "purple", 
-                                "Metabolism-Related Gene Expression" = "orange")) +
+  scale_color_manual(values = c("Glucocorticoid Receptor Expression" = "purple" 
   scale_x_continuous(breaks = seq(0, 24, by = 6), labels = c("00:00", "06:00", "12:00", "18:00", "00:00")) +
   theme_minimal() +
   theme(legend.title = element_blank())
+
